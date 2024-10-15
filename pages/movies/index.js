83be02eaafab12ec9636bd7826225d9050cb6877
Niklas Röttger <niklas.roettger@outlook.de>
2024-10-15 10:13:56 +0200

    add session code

--- pages/movies/index.js
+++ pages/movies/index.js
@@ -1,14 +1,30 @@
 import Link from "next/link";
+import { movies } from "../../lib/data";
+import { useRouter } from "next/router";
 
 export default function Movies() {
+  const router = useRouter();
+
   return (
     <>
       <h1>List of Movies</h1>
       <ul>
-        <li>
-          <Link href={`/movies/aquaman`}>Aquaman</Link>
-        </li>
+        {movies.map((movie) => (
+          <li key={movie.slug}>
+            <Link href={`/movies/${movie.slug}`}>{movie.title}</Link>
+          </li>
+        ))}
       </ul>
+      <button
+        onClick={() => {
+          if (confirm("Are you Aquaman?")) {
+            router.push("/movies/aquaman");
+          }
+        }}
+        type="button"
+      >
+        Are you Aquaman?
+      </button>
     </>
   );
 }
